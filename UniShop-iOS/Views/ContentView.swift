c8295117df0db80bcd42f4100eb33213d59bbc13
AlejoG10-uniandes <alejogonza99@hotmail.com>
2023-10-03 20:16:56 -0500

    fix navbar position

--- UniShop-iOS/Views/ContentView.swift
+++ UniShop-iOS/Views/ContentView.swift
@@ -27,7 +27,7 @@ struct ContentView: View {
             NavbarView { index in
                 selectedTab = index
             }
-            .position(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height - 105)
+            .position(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height - 90)
         }
         .hideNavigationBar()
     }
