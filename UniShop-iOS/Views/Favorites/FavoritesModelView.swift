ed269db60c731231112a5aef0a55b27773b34ea5
Maria <m.cely@uniandes.edu.co>
2023-10-19 13:45:28 -0500

    arreglos sprint 2

---
+++ UniShop-iOS/Views/Favorites/FavoritesModelView.swift
@@ -0,0 +1,8 @@
+//
+//  FavoritesModelView.swift
+//  UniShop-iOS
+//
+//  Created by Majo Cely on 15/10/23.
+//
+
+import Foundation
---
+++ UniShop-iOS/Views/Posts/PostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostViewModel.swift
+//  UniShop-iOS
+//
+//  Created by Majo Cely on 15/10/23.
+//
+
+import Foundation
