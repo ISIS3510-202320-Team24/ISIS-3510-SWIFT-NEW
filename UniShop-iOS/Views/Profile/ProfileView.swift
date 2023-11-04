e7bf5b5530dc85ce1fe03dff3e6f8ba1c6dd7c8d
jphernandezr1 <jp.hernandezr1@uniandes.edu.co>
2023-11-03 19:17:00 -0500

    mall tweak in caching

--- UniShop-iOS/Views/Profile/ProfileView.swift
+++ UniShop-iOS/Views/Profile/ProfileView.swift
@@ -129,7 +129,7 @@ struct ProfileView: View {
                 defaults.removeObject(forKey: "userPosts")
                 defaults.removeObject(forKey: "allProducts")
                 defaults.removeObject(forKey: "recommendedProducts")
-                defaults.removeObject(forKey: "userFavoriteProducts")
+                defaults.removeObject(forKey: "userFavorites")
                 
                 
        
